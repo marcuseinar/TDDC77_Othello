e76b970db2afd1c32a0359c8d8648b110221af17
marei265 <marei265@student.liu.se>
2014-07-04 23:46:38 +0200

    Started implementing Game class. Ideas saved as comments. Nothing definite.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import Player.IPlayer;
 import UserInterface.IUserInterface;
+import com.sun.tools.javac.util.Pair;
 
 /**
  * Created by einar on 2014-06-29.
@@ -8,11 +9,35 @@ import UserInterface.IUserInterface;
  */
 public class Game {
     private Board board;
-    private IPlayer IPlayer1;
-    private IPlayer IPlayer2;
-    private IUserInterface ui;
+    private IPlayer player1;
+    private IPlayer player2;
+    private IUserInterface userInterface;
 
-    public Game(){
 
+    public Game(IPlayer player1, IPlayer player2, IUserInterface userInterface){
+        this.board = new Board();
+        this.player1 = player1;
+        this.player2 = player2;
+        this.userInterface = userInterface;
+    }
+
+    public void run(){
+        while(!gameOver()){
+            getPlayerTurn().getMove(this.userInterface); //TODO: decide return type
+            //make move
+            //if next player has > 1 valid moves, change turn
+            //else if same player has > 1 moves restart loop
+            //else gameOver()
+        }
+        //Display winner:
+        //Wait for ui input
+    }
+
+    private IPlayer getPlayerTurn() {
+        return player1;
+    }
+
+    private boolean gameOver() { //TODO: Make this a boolean field instead?
+        return false;
     }
 }
