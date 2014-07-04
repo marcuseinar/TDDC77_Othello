389698c13d5c582c047fb2d5a4c849f40bd3e83e
marei265 <marei265@student.liu.se>
2014-07-04 23:47:28 +0200

    Dummy methods created to start Game class development.

--- src/Player/IPlayer.java
+++ src/Player/IPlayer.java
@@ -1,8 +1,11 @@
 package Player;
 
+import UserInterface.IUserInterface;
+
 /**
  * Created by einar on 2014-07-04.
  * This class is the base interface that all player classes must implement.
  */
 public interface IPlayer {
+    void getMove(IUserInterface userInterface);
 }
