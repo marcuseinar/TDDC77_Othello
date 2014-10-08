5372e1cb26b7499f574e07cd8a819ba6525ee5ba
marei265 <marei265@student.liu.se>
2014-10-08 06:49:01 +0200

    Added main file

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,17 @@
-//import Controllers.Game;
-//import Models.Board;
-//import Player.IPlayer;
-//import Player.HumanPlayer;
-//import Player.MinMaxAIPlayer;
-//import UserInterface.AbstractUserInterface;
-//import UserInterface.TextUserInterface.TextUserInterface;
-//
-///**
-// * Created by einar on 2014-07-07.
-// */
-//public class Main {
-//    public static void main(String args[]){
-//        Board board = new Board();
-//        AbstractUserInterface ui = new TextUserInterface(board);
-//        IPlayer p1 = new HumanPlayer(ui);
-//        IPlayer p2 = new MinMaxAIPlayer(20);
-//        Game g = null;
-//        try {
-//            //g = new Game(p1,p2,ui, board);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        g.run();
-//    }
-//}
+import UserInterface.GUI.GraphicalUserInterface;
+
+import javax.swing.*;
+
+/**
+/ * Created by einar on 2014-07-07.
+/ */
+public class Main {
+    public static void main(String args[]){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GraphicalUserInterface();
+            }
+        });
+    }
+}
